75875fbf19b6fecb807c12ead654c79dffc124cf
Brooke Kullberg and Colton Lacey <colton@>
2019-06-11 11:58:44 -0700

    add base files

---
+++ src/app/models/fly.model.ts
@@ -0,0 +1,3 @@
+export class Fly {
+  constructor(public name: string, public description: string, public have: boolean, public wishList: boolean, public packList: boolean, public image: string) {}
+}
